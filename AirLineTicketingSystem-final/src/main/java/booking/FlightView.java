49b0abbd0424f294a97e4e916c3f5618d99ad458
Bhavani Sathi <bhavani.s@comakeit.com>
2021-11-15 11:42:45 +0530

    wrong commit

--- AirLineTicketingSystem-final/src/main/java/booking/FlightView.java
+++ AirLineTicketingSystem-final/src/main/java/booking/FlightView.java
@@ -1,27 +1,32 @@
 package booking;
-import java.io.IOException;  
-import java.io.PrintWriter;  
-  
-import javax.servlet.ServletException;  
-import javax.servlet.annotation.WebServlet;  
-import javax.servlet.http.HttpServlet;  
-import javax.servlet.http.HttpServletRequest;  
-import javax.servlet.http.HttpServletResponse; 
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.sql.*;
 import java.util.List;
-	@WebServlet("/BookingView")  
-	public class FlightView extends HttpServlet {  
-		protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-			res.setContentType("text/html");
-			PrintWriter out = res.getWriter();
-			List<Book> list = BookDao.getAllBooks();
-			out.print("<head><link rel=\"stylesheet\" href=\"index.css\" /></head>");
-			out.print("<table border='1' width='100%'");
-			out.print(
-					"<tr><th>BookID</th><th>Source</th><th>Destination</th><th>No of passengers</th><th>Date of journey</th></tr>");
-			for (Book s : list) {
-				out.print("<tr><td>" + s.getBookid() + "</td><td>" + s.getSource() + "</td><td>" + s.getDestination() + "</td><td>"
-						+ s.getNoofpassengers() + "</td><td>" + s.getDateofjourney() +"</td></tr>");
-			}
-		}  
-	 }  
+
+@WebServlet("/BookingView")
+public class FlightView extends HttpServlet {
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		res.setContentType("text/html");
+		PrintWriter out = res.getWriter();
+		List<Book> list = BookDao.getAllBooks();
+		out.print("<head><style>" + "" + "table{" + "color:white;" + "background-color: rgba(0, 0, 0, 0.493);" + "}"
+				+ "#b{" + "background-image: url(\"ticket.png\");" + "background-position: center center;"
+				+ "background-attachment: fixed;" + "background-size: cover;" + "backdrop-filter: blur(5px);" + "" + "}"
+				+ "" + "</style></head>");
+		out.print("<table border='1' width='100%'");
+		out.print(
+				"<tr><th>BookID</th><th>Source</th><th>Destination</th><th>No of passengers</th><th>Date of journey</th></tr>");
+		for (Book s : list) {
+			out.print("<tr><td>" + s.getBookid() + "</td><td>" + s.getSource() + "</td><td>" + s.getDestination()
+					+ "</td><td>" + s.getNoofpassengers() + "</td><td>" + s.getDateofjourney() + "</td></tr>");
+		}
+	}
+}
--- AirLineTicketingSystem-final/src/main/java/passengers/PassengerView.java
+++ AirLineTicketingSystem-final/src/main/java/passengers/PassengerView.java
@@ -11,19 +11,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/PassengerView")
-public class PassengerView  extends HttpServlet{
+public class PassengerView extends HttpServlet {
 	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		res.setContentType("text/html");
 		PrintWriter out = res.getWriter();
-		
+ 
 		List<Passenger> list = PassengerDao.getAllPassengers();
-		out.print("<table border='1' width='100%'");
-		out.print(
-				"<tr><th>Passenger Id</th><th>Name</th><th>Age</th><th>Gender</th></tr>");
-		for (Passenger s : list) {
-			out.print("<tr><td>" + s.getP_id() + "</td><td>" + s.getP_name() + "</td><td>" + s.getP_age() + "</td><td>"+ s.getP_gender()+"</td>"
-					+ "</tr>");
+//		String cssTag="<link rel='stylesheet' type='text/css' href='sty1.css'>";
+			    out.println("<html>");
+//			    out.println("<head><title>Title Name</title>"+cssTag+"</head>");
+			    out.println("<body>");
+//		out.print("<table>");
+//		out.print("<tr><th>Passenger Id</th><th>Name</th><th>Age</th><th>Gender</th></tr>");
+//		for (Passenger s : list) {
+//			out.print("<tr><td>" + s.getP_id() + "</td><td>" + s.getP_name() + "</td><td>" + s.getP_age() + "</td><td>"
+//					+ s.getP_gender() + "</td>" + "</tr>");
+//		}
+//		out.print("</table></body></html>");
 	}
 }
-}
-		
